061d9eb591d9baf691c41a2438caf585fa03094a
Sajjad Ahmad <jogimar14@gmail.com>
2020-07-31 01:20:49 +0500

    Created and coded block for yield of sorting method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,28 +1,21 @@
 def sorting
-  my_array = [10, 0, 9, 1, 8, 2, 7, 3, 6, 4, 5]
+  my_array = [10, 0, 9, 1, 8, 2, 7, 3, 6, -4, 5]
   yield my_array
 end
 
-sorting {|x| print x}
-
-
-
-=begin
-
-my_arr = [2, 1, 3, 7, 9, -1, 23, 22, 12, 11]
-n = my_arr.length
-
-my_arr.length.times do |arr_1|
-  (my_arr.length - (arr_1 + 1)).times do |arr|
-    next unless my_arr[arr + 1] != nil
-
-    next unless my_arr[arr] > my_arr[arr + 1]
-
-    swap = my_arr[arr + 1]
-    my_arr[arr + 1] = my_arr[arr]
-    my_arr[arr] = swap
+sorting do |my_arr|
+  my_arr.length.times do |arr_1|
+    (my_arr.length - (arr_1 + 1)).times do |arr|
+      if my_arr[arr + 1] != nil
+        if my_arr[arr] > my_arr[arr + 1]
+          swap = my_arr[arr + 1]
+          my_arr[arr + 1] = my_arr[arr]
+          my_arr[arr] = swap
+        end
+      end
+    end
   end
+  print my_arr
 end
-print my_arr
 
-=end
+
