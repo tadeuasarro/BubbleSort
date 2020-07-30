c60c3dc0f5914e8a0973eff8b6ceff4522e82302
tadeuasarro <tadeutas@gmail.com>
2020-07-30 17:06:08 -0300

    Try to work on methods

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,31 +1,13 @@
-# my_array = [5, 1, 4, 2, 3, 10, 50, 90, 10, 15, 74, 36, 0, 150, 60]
-#
-# print my_array
-# puts ''
-#
-# my_array.length.times do |y|
-#
-#   (my_array.length - (y + 1)).times do |x|
-#
-#     if my_array[x + 1] != nil
-#       if my_array[x] > my_array[x + 1]
-#
-#         pivot = my_array[x]
-#         my_array[x] = my_array[x + 1]
-#         my_array[x + 1] = pivot
-#
-#       end
-#     else
-#       puts 'It is not set'
-#     end
-#
-#   end
-#
-#
-# end
-#
-# print my_array
-# puts ''
+def sorting
+  my_array = [10, 0, 9, 1, 8, 2, 7, 3, 6, 4, 5]
+  yield my_array
+end
+
+sorting {|x| print x}
+
+
+
+=begin
 
 my_arr = [2, 1, 3, 7, 9, -1, 23, 22, 12, 11]
 n = my_arr.length
@@ -42,3 +24,5 @@ my_arr.length.times do |arr_1|
   end
 end
 print my_arr
+
+=end
