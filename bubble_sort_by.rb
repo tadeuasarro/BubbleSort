9fa401b208b96429f4542529fd033289bd925847
Sajjad Ahmad <jogimar14@gmail.com>
2020-07-31 02:19:56 +0500

    Removed commented code

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -26,20 +26,3 @@ end
 puts "Sorted array:"
 print my_array
 puts ''
-
-
-=begin
-names = ["pakistan", "is", "mine", "country"]
-print names
-names.length.times do |name_1|
-    (names.length - (name_1 + 1)).times do |name|
-        if names[name + 1] != nil
-            if names[name].length > names[name + 1].length
-                swap = names[name + 1]
-                names[name + 1] = names[name]
-                names[name] = swap
-            end
-        end
-    end
-end
-=end
