0d7f5c0f081d9893b9dd2daad18fb08bf36584da
tadeuasarro <tadeutas@gmail.com>
2020-07-30 12:49:26 -0300

    bubble_sort first version

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,1 +1,28 @@
-puts "Hello world!!"
+my_array = [5, 1, 4, 2, 3, 10, 50, 90, 10, 15, 74, 36, 0, 150, 60]
+
+print my_array
+puts ''
+
+my_array.length.times do |y|
+
+  (my_array.length - (y + 1)).times do |x|
+
+    if my_array[x + 1] != nil
+      if my_array[x] > my_array[x + 1]
+
+        pivot = my_array[x]
+        my_array[x] = my_array[x + 1]
+        my_array[x + 1] = pivot
+
+      end
+    else
+      puts 'It is not set'
+    end
+
+  end
+
+
+end
+
+print my_array
+puts ''
