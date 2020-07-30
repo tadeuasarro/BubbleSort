4b527017da5290efc2359fdaa11a43faebabd884
Sajjad Ahmad <jogimar14@gmail.com>
2020-07-31 00:39:29 +0500

    Created the code for bubble_sort_by method

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,1 +1,13 @@
-puts "Hello world!!"
+names = ["pakistan", "is", "mine", "country"]
+print names
+names.length.times do |name_1|
+    (names.length - (name_1 + 1)).times do |name|
+        if names[name + 1] != nil
+            if names[name].length > names[name + 1].length
+                swap = names[name + 1]
+                names[name + 1] = names[name]
+                names[name] = swap
+            end
+        end
+    end
+end
