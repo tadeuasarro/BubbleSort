ba998918967a790dc6e5815d2398e12d87111a2a
Sajjad Ahmad <jogimar14@gmail.com>
2020-07-30 22:36:42 +0500

    Tried working with sorting logic

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,28 +1,44 @@
-my_array = [5, 1, 4, 2, 3, 10, 50, 90, 10, 15, 74, 36, 0, 150, 60]
+# my_array = [5, 1, 4, 2, 3, 10, 50, 90, 10, 15, 74, 36, 0, 150, 60]
+#
+# print my_array
+# puts ''
+#
+# my_array.length.times do |y|
+#
+#   (my_array.length - (y + 1)).times do |x|
+#
+#     if my_array[x + 1] != nil
+#       if my_array[x] > my_array[x + 1]
+#
+#         pivot = my_array[x]
+#         my_array[x] = my_array[x + 1]
+#         my_array[x + 1] = pivot
+#
+#       end
+#     else
+#       puts 'It is not set'
+#     end
+#
+#   end
+#
+#
+# end
+#
+# print my_array
+# puts ''
 
-print my_array
-puts ''
+my_arr = [2, 1, 3, 7, 9, -1, 23, 22, 12, 11]
+n = my_arr.length
 
-my_array.length.times do |y|
+my_arr.length.times do |arr_1|
+  (my_arr.length - (arr_1 + 1)).times do |arr|
+    next unless my_arr[arr + 1] != nil
 
-  (my_array.length - (y + 1)).times do |x|
+    next unless my_arr[arr] > my_arr[arr + 1]
 
-    if my_array[x + 1] != nil
-      if my_array[x] > my_array[x + 1]
-
-        pivot = my_array[x]
-        my_array[x] = my_array[x + 1]
-        my_array[x + 1] = pivot
-
-      end
-    else
-      puts 'It is not set'
-    end
-
+    swap = my_arr[arr + 1]
+    my_arr[arr + 1] = my_arr[arr]
+    my_arr[arr] = swap
   end
-
-
 end
-
-print my_array
-puts ''
+print my_arr
