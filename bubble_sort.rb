6901d41918ea3607e47fa98ecf7177655cd1f23d
Sajjad Ahmad <jogimar14@gmail.com>
2020-08-03 20:51:21 +0500

    Removed error in RSpec

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,13 +2,12 @@ def sorting(array)
   array.length.times do |arr1|
     (array.length - (arr1 + 1)).times do |arr|
       next if array[arr + 1].nil?
-      
-        if array[arr] > array[arr + 1]
-          
-          swap = array[arr + 1]
-          array[arr + 1] = array[arr]
-          array[arr] = swap
-        end
+
+      if array[arr] > array[arr + 1]
+        swap = array[arr + 1]
+        array[arr + 1] = array[arr]
+        array[arr] = swap
+      end
     end
   end
   array
