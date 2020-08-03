af64f1bb6e726e55e11190cb0580bddd475ed353
tadeuasarro <tadeutas@gmail.com>
2020-08-03 13:34:49 -0300

    Linter run almost finished, checking what's left

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,23 +1,20 @@
 def bubble_sort_by(array)
   array.length.times do |x|
-
     array.length - (x + 1).times do |y|
-
-      unless array[y + 1].nil?
+      next if array[y + 1].nil?
         case yield array[y], array[y + 1]
         when 1
-        pivot = array[y]
-        array[y] = array[y + 1]
-        array[y + 1] = pivot
+          pivot = array[y]
+          array[y] = array[y + 1]
+          array[y + 1] = pivot
         end
-      end
     end
   end
 end
 
-my_array = ['why', 'howdy', 'constitution', 'hi', 'whatup']
+my_array = %w[why howdy constitution hi whatup]
 
-puts "Unsorted array:"
+puts 'Unsorted array:'
 print my_array
 puts ''
 
@@ -25,6 +22,6 @@ bubble_sort_by(my_array) do |x, y|
 	x.length <=> y.length
 end
 
-puts "Sorted array:"
+puts 'Sorted array:'
 print my_array
 puts ''
